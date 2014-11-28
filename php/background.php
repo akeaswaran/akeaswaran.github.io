9235e0e11902a0d1c64db8cfb604f67cf0808802
akeaswaran <akeaswaran@me.com>
2014-11-27 21:16:37 -0500

    updated site; removed blog bc unnecessary

---
+++ php/background.php
@@ -0,0 +1,61 @@
+<?php
+ echo '<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="background.css">
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+    </head>
+    <title>Background - Akshay Easwaran</title>
+    <body>
+        <div id="headerTitle">
+            <h1>Akshay Easwaran</h1>
+        </div>
+
+        <div id="navContainer">
+            <a href="index.html" id="homeTab">Home</a>
+            <a href="background.html" id="backgroundTab">Background</a>
+            <a href="projects.html" id="projectsTab">Projects</a>
+            <a href="keynote/index.html" id="keynoteTab">Keynote</a>
+        </div>
+
+         <div id="educationContainer">
+            <h3>Education</h3>
+            <div id="eduText">
+                <p>In four years at Milton High School, I have taken multiple AP and Honors Courses in all subjects, with a total of 11 AP-level classes, 9 additional Honors-level classes, and 2 dual-enrollment college courses. Among them:</p>
+                    <li>AP Statistics</li>
+                    <li>AP Psychology</li>
+                    <li>AP Macroeconomics</li>
+                    <li>AP US History</li>
+                    <li>AP Chemistry</li>
+                    <li>AP Calculus BC</li>
+                    <li>AP Physics B</li>
+                    <li>AP English Language</li>
+                    <li>AP Computer Science</li>
+                    <li>AP World History</li>
+                    <li>AP Human Geography</li>
+                    <li>Georgia Tech - Calculus II-III</li>
+                    <li>Georgia Perimeter College - English Literature II-III</li>
+            </div>
+        </div>
+
+        <div id="techBackgroundContainer">
+            <h3>Technical Background</h3>
+            <div id="backgroundText">
+                <p>I am self-taught and well-versed in the following programming languages:</p>
+                <li>Objective-C</li>
+                <li>Java</li>
+                <li>Apex/VisualForce</li>
+                <li>HTML/CSS</li>
+            </div>
+        </div>
+
+        <div id="employmentContainer">
+            <h3>Employment History</h3>
+            <div id="employmentText">
+                <li id="csURL">July - September 2013: <a href="http://cloudsherpas.com/">CloudSherpas, Inc.</a> - iOS Developer (Intern)</li>
+                <li id="anmURL">May - July 2014: <a href="http://appsnmotion.com/">AppsNMotion, Inc.</a> - iOS Developer (Intern)</li>
+                <li id="pardotURL">July 2014 - May 2015: <a href="http://pardot.com/">Salesforce Pardot, Inc.</a> - Software Engineer (Intern)</li>
+            </div>
+        </div>
+    </body>
+</html>';
