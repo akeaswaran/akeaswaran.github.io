7259b19311295cc29560d52a5fc9bf800148592b
Akshay Easwaran <akeaswaran@me.com>
2020-06-30 23:20:47 -0400

    updating script and articles

---
+++ _posts/2020-06-30-georgia-tech-football-film-school---expected-points.md
@@ -0,0 +1,6 @@
+---
+layout: post
+title: "Georgia Tech Football: Film School - Expected Points"
+description: "Advanced football nerdery ahead."
+permalink: https://www.fromtherumbleseat.com/2020/6/30/21307811/georgia-tech-football-film-school-expected-points-college-football-epa-per-play-advanced-stats
+---
--- refresh_ftrs.py
+++ refresh_ftrs.py
@@ -6,29 +6,30 @@ import feedparser
 import re
 from os import listdir
 from os.path import isfile, join
-from datetime import datetime
+import time
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 
-print 'Starting update of blog with recent FTRS posts.'
-print 'Checking date of last published blog post...'
+print('Starting update of blog with recent FTRS posts.')
+print('Checking date of last published blog post...')
 onlyfiles = [f for f in listdir('./_posts') if isfile(join('./_posts', f))]
 onlyfiles.sort()
 last_title = onlyfiles[-1]
 m = re.search('^([0-9]+\-[0-9]+\-[0-9]+)', last_title)
 if m:
     found = m.group(1)
-    last_date = datetime.strptime(found, '%Y-%m-%d').date()
-    print 'Last blog post date: ' + found
+    last_date = time.strptime(found, '%Y-%m-%d')
+    print('Last blog post date: ' + found)
 
-    print 'Retrieving latest FTRS articles written by self...'
+    print('Retrieving latest FTRS articles written by self...')
     d = feedparser.parse('https://www.fromtherumbleseat.com/authors/akeaswaran/rss')
     self_articles = d.entries # filter(lambda x: {'name' : 'Akshay Easwaran'} in x.authors, d.entries)
 
-    print 'Parsing FTRS articles...'
+    print('Parsing FTRS articles...')
     for a in self_articles:
-        if a.published_parsed > last_date.isoformat():
-            print 'Formatting and writing \"' + a.title + '\" to _posts...'
+        # print(a.published_parsed)
+        if a.published_parsed > last_date:
+            print('Formatting and writing \"' + a.title + '\" to _posts...')
             title = a.title
             if len(title) > 56:
                 title = title[:56].rstrip() + "..."
@@ -47,9 +48,9 @@ if m:
             # Write the article's metadata to a Markdown file for Jekyll publishing.
             f = open(new_file_path, 'w')
             markdown_format = '---\nlayout: post\ntitle: "' + title + '"\ndescription: "' + dek + '"\npermalink: ' + link + '\n---'
-            f.write(markdown_format.encode('utf-8'))
-            print 'Wrote \"' + a.title + '\" to _posts.'
+            f.write(markdown_format)
+            print('Wrote \"' + a.title + '\" to _posts.')
 
-    print 'Done updating blog with new FTRS articles.'
+    print('Done updating blog with new FTRS articles.')
 else:
-    print 'ERROR: unable to determine last post date.'
+    print('ERROR: unable to determine last post date.')
